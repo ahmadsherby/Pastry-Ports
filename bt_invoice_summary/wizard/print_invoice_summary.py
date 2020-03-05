bf91079e730c5d6f19f5b025c219b676c12cddb7
telenocodoo <45532030+telenocodoo@users.noreply.github.com>
2020-03-05 13:26:15 +0300

    Update print_invoice_summary.py

--- bt_invoice_summary/wizard/print_invoice_summary.py
+++ bt_invoice_summary/wizard/print_invoice_summary.py
@@ -11,7 +11,7 @@ import datetime
 class PrintInvoiceSummary(models.TransientModel):
     _name = "print.invoice.summary"
     
-    @api.model
+
     def _get_from_date(self):
         company = self.env.user.company_id
         current_date = datetime.date.today()
